21b4f3bd23c7bfc1464d0f56a75e521d6252372d
Adam Stephenson <adstep@microsoft.com>
2025-12-04 21:10:33 -0800

    init commit

---
+++ ExampleLibrary/Parser.cs
@@ -0,0 +1,27 @@
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ExampleLibrary;
+
+using AdaptiveCards.ObjectModel.WinUI3;
+using System;
+
+public sealed class Parser
+{
+    public static void Parse()
+    {
+        AdaptiveCardParseResult result = AdaptiveCard.FromJsonString(string.Empty);
+
+        if (result.Errors.Count != 1)
+        {
+            throw new InvalidOperationException("Unexpected number of errors.");
+        }
+
+        if (result.Errors[0].StatusCode != ErrorStatusCode.InvalidJson)
+        {
+            throw new InvalidOperationException("Unexpected error code.");
+        }
+
+        Console.WriteLine("AdaptiveCard produced expected parse error.");
+    }
+}
---
+++ ExampleOldApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExampleOldLibrary;
+
+Console.WriteLine("Running parsing test...");
+
+Parser.Parse();
+
